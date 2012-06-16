c3ea104fac61106099d670e377579ecf4ec2d38f
Wes <hi@wes-waugh.com>
2012-06-15 17:26:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+*Chordimate* animates chords, modes, and scales on a guitar fretboard.
+
+I don't claim that traditional music notation is dead, but its learning curve
+is steep. Tablature is more acessable, but it's limited:
+
+* It only notates one fret position per string at a time
+* It tries to mimic traditional notation where each note is prescribed. This
+  can lead to an awful ocean of numerals, wherein locating differences is
+  difficult.
+* It's very difficult to synthesize any scale from the notes.
+* It doesn't offer a particularly good way to indicate changes.
+
+*Chordimate* tries to solve these things and encourage improvisation even if
+you don't know Myxolyidian mode or how to spell a Cmin#7b9.
+
+The display is something like [Guitar Chord Generator]() but this project
+doesn't claim to be an authority on any theory beyond the dead-simple. The idea
+here is to assist the creative process, not to suggest or impose anything. The
+user could, for example:
+
+1. Write a song
+2. Spell the chords
+3. Fit those chords into scales or modes
+4. Type those chords into *Chordimate* and line up the timing.
+5. Send that score to the rest of the band
+
+# The Tech
+Depends only on jQuery. Draws and animates with CSS.
+
+
---
+++ main.js
@@ -0,0 +1,43 @@
+!function($) {
+    var up_count = ['C', 'C#', 'D', 'D#', 'E', 'F', 
+                    'F#', 'G', 'G#', 'A', 'A#', 'B'],
+        dn_count = ['C', 'Db', 'D', 'Eb', 'E', 'F', 
+                    'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+
+    function unspell(note_name) {
+        var ord = up_count.indexOf(note_name.toLowerCase())
+        if(ord > -1) return ord
+        var ord = dn_count.indexOf(note_name.toLowerCase())
+        if(ord > -1) return ord
+        return -1
+    }
+
+    function spell(ord, up) {
+        return (up && up_count || dn_count)[ord]
+    }
+
+    methods = {
+        init: function(opts) {
+            var settings = $.extend({
+            }, opts)
+            return this
+        },
+        change: function(notes) {
+
+            return this
+        }
+    }
+    $.fn.chordimate = function(method) {
+        if(methods[method]) {
+            return methods[method].apply(
+                this, Array.prototype.slice.call(arguments, 1))
+        }
+        else if(typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments)
+        }
+        else {
+            $.error(method + ' does not exist on jQuery.chordimate')
+        }
+    }
+}(jQuery)
+
